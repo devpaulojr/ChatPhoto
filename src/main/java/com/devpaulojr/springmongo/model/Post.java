51f519b26534234a078782c94275e17f9e508431
devpaulojr <paulosantanajunior133@gmail.com>
2025-01-15 22:48:46 -0300

    Create Post + PostRepository

---
+++ src/main/java/com/devpaulojr/springmongo/model/Post.java
@@ -0,0 +1,27 @@
+package com.devpaulojr.springmongo.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Document(collection = "post")
+public class Post implements Serializable {
+
+    @Id
+    private String id;
+
+    private LocalDate date;
+    private String title;
+    private String body;
+
+    private User author;
+
+}
