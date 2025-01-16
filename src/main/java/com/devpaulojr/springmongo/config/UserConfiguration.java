a4a250e82ec6b50f0194f7bc98ebf67625590fbf
devpaulojr <paulosantanajunior133@gmail.com>
2025-01-15 23:05:38 -0300

    Instanciando a classe Post

--- src/main/java/com/devpaulojr/springmongo/config/UserConfiguration.java
+++ src/main/java/com/devpaulojr/springmongo/config/UserConfiguration.java
@@ -1,11 +1,14 @@
 package com.devpaulojr.springmongo.config;
 
+import com.devpaulojr.springmongo.model.Post;
 import com.devpaulojr.springmongo.model.User;
+import com.devpaulojr.springmongo.repositories.PostRepository;
 import com.devpaulojr.springmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 @Configuration
@@ -14,30 +17,63 @@ public class UserConfiguration implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
-        var user1 = new User(null,
+        var paulo = new User(
+                null,
                 "paulo",
                 "paulo@gmail.com",
                 "83940028922",
                 "400289");
 
-        var user2 = new User(null,
+        var clara = new User(
+                null,
                 "clara",
                 "clara@gmail.com",
                 "8935421247",
                 "880dsd");
 
-        var user3 = new User(null,
+        var carlos = new User(
+                null,
                 "carlos",
                 "carlos@gmail.com",
                 "83976634566",
                 "Carlos124");
 
-        userRepository.saveAll(Arrays.asList(user1, user2, user3));
+        var post1 = new Post(
+                null,
+                LocalDate.of(2004, 1, 12),
+                "Cachorros pulando",
+                "Passeando com cachorros alegres",
+                paulo
+        );
+
+
+        var post2 = new Post(
+                null,
+                 LocalDate.now(),
+                "Foto família",
+                "Foto com a galera, geral!!",
+                carlos
+        );
+
+        var post3 = new Post(
+                null,
+                LocalDate.of(2012, 7,7),
+                "+1 foto de cachorro",
+                "Novamente passeando com cachorros alegres",
+                paulo
+        );
+
+        userRepository.saveAll(Arrays.asList(paulo, clara, carlos));
+        postRepository.saveAll(Arrays.asList(post1, post2, post3));
     }
 }
